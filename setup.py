fd2a948305bf51e83f5392bce22af15c4c09152e
Tom Elliott <tom.elliott@nyu.edu>
2021-06-17 17:46:45 -0500

    include dependencies for fuzzywuzzy and python-slugify

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['airtight', 'pathvalidate', 'python-magic', 'requests', 'regex', 'textnorm'],
+    install_requires=['airtight', 'fuzzywuzzy[speedup]', 'pathvalidate', 'python-magic', 'python-slugify', 'requests', 'regex', 'textnorm'],
     python_requires='>=3.9.5'
 )
